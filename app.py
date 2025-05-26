6d9efd905b706b7819e38a1058ec43812e3acd66
Sonika Arora <sonika@berkeley.edu>
2025-05-26 12:41:31 -0700

    modified sms cron job and gitingore

--- app.py
+++ app.py
@@ -495,7 +495,7 @@ def scheduled_daily_prompt_job():
             app.logger.error(f"Scheduler: Error fetching subscribed users: {e}")
 
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(scheduled_daily_prompt_job, 'cron', hour=22, minute=28)
+scheduler.add_job(scheduled_daily_prompt_job, 'cron', hour=18, minute=30)
 
 # --- Flask CLI Commands ---
 @app.cli.command("init-db") # This command is now a misnomer, as there's no SQL DB to init tables for.
